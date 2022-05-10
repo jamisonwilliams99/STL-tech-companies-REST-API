9a1a586b77bc7046ad5f628e82037b7cf628c0ef
jamisonwilliams99 <69990463+jamisonwilliams99@users.noreply.github.com>
2022-05-10 10:54:52 -0500

    refactored webscraper code; modularized individual logic operations to functions

--- webscraper.py
+++ webscraper.py
@@ -7,21 +7,8 @@ def extract_name_from_url(url):
     name = name.replace("_", " ")
     return name
 
-url = "https://growjo.com/city/St_Louis"
-api_url = "http://127.0.0.1:8080/api/create_company"
 
-result = requests.get(url)
-
-doc = BeautifulSoup(result.text, "html.parser")
-
-tbody = doc.tbody
-trs = tbody.contents
-
-companies = []
-for tr in trs:
-    if len(tr.contents) == 1:
-        continue
-    
+def extract_company_data(tr):
     company_name_td, industry_td, funding_td  = tr.contents[1], tr.contents[2], tr.contents[3]
     num_employees_td, employee_growth_td, revenue_td = tr.contents[4], tr.contents[5], tr.contents[6]
 
@@ -33,14 +20,46 @@ for tr in trs:
     revenue = revenue_td.text
 
     company_data = {"name": company_name, "industry": industry, "funding": funding, "employees": num_employees, "employeegrowth": employee_growth, "revenue": revenue}
-    companies.append(company_data)
-
-for company in companies:
-    response = requests.post(api_url, json=company)
-    response.json()
-    response.status_code
+    return company_data
 
 
+def scrape_site(http_response):
+    doc = BeautifulSoup(http_response.text, "html.parser")
+
+    tbody = doc.tbody
+    company_trs = tbody.contents
+
+    companies = []
+    for company_tr in company_trs:
+        if len(company_tr.contents) == 1:
+            continue
+        
+        company_data = extract_company_data(company_tr)
+        companies.append(company_data)
+    
+    return companies
+
+
+def http_post_companies(companies, api_url):
+    for company in companies:
+        response = requests.post(api_url, json=company)
+        response.json()
+        response.status_code
+
+
+def main():
+    url = "https://growjo.com/city/St_Louis"
+    api_url = "http://127.0.0.1:8080/api/create_company"
+
+    result = requests.get(url)
+
+    companies = scrape_site(result)
+
+    http_post_companies(companies, api_url)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
